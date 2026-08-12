838fc82a33ed5ff3c8eb56b14d14ca1666825b8f
hohoplay <hohoplay@users.noreply.github.com>
2026-08-12 04:32:45 +0000

    🚀 Deploy add8e3acccf201f60cf8a8541daee14a809b5402 [cache-bust] add8e3acccf201f60cf8a8541daee14a809b5402

--- danang_guide/sw.js
+++ danang_guide/sw.js
@@ -26,7 +26,7 @@ const PRECACHE_URLS = [
     'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://fonts.googleapis.com/css2?family=Pretendard:wght@300;400;500;600;700&display=swap'
+    'https://cdnjs.cloudflare.com/ajax/libs/pretendard/1.3.9/static/pretendard.min.css'
 ];
 
 self.addEventListener('install', (event) => {
